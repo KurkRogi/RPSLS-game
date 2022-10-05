9864ba70d434be7b06faa4a0d4739c710c5b9e4d
KurkRogi <igor.k.kruk@gmail.com>
2022-10-05 23:55:26 +0000

    Add game controls handler and restore data from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,48 @@
-window.addEventListener("load", mainLoop);
 
-function mainLoop() {
+// Global variables
 
-// Attach event handlers to navigation buttons
+const RANDOM_BATCH = 100;
+const RANDOM_ORG_URL = `https://www.random.org/integers/`;
 
-document.getElementById("game-rules-button").addEventListener( 'click', e => {
-    const screen = document.getElementById("rules-screen");
-    screen.classList.toggle("visible");
-});
+var areRandomsLocal;
+var randoms;
+var stats;
 
-document.getElementById("game statistics-button").addEventListener( 'click', e => {
-    const screen = document.getElementById("stats-screen");
-    screen.classList.toggle("visible");
-});
+window.addEventListener("load", initLoop);
 
-for (let i of document.getElementsByClassName("close-icon")) {
-    i.addEventListener('click', e => {
-        e.target.parentElement.parentElement.classList.toggle("visible");
-    })
-}
+function initLoop() {
+
+    // Attach event handlers to navigation buttons
+
+    document.getElementById("game-rules-button").addEventListener( 'click', e => {
+        const screen = document.getElementById("rules-screen");
+        screen.classList.toggle("visible");
+    });
+
+    document.getElementById("game statistics-button").addEventListener( 'click', e => {
+        const screen = document.getElementById("stats-screen");
+        screen.classList.toggle("visible");
+    });
+
+    for (let i of document.getElementsByClassName("close-icon")) {
+        i.addEventListener('click', e => {
+            e.target.parentElement.parentElement.classList.toggle("visible");
+        })
+    }
+
+    // Attach event handler to players decision buttons
+
+    document.getElementById("decision").addEventListener("click", gameThrow);
+
+    // Pull data from local storage
+
+    randoms = JSON.parse(window.localStorage.getItem('randoms'));
+    stats = JSON.parse(window.localStorage.getItem('stats'));
+
+    // If there are no randoms generate local set
+
+    if (randoms === null) {
+        randoms = generateLocalRandoms(RANDOM_BATCH)
+        areRandomsLocal = true;
+    }
 }
